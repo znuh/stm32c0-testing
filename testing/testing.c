d3e4bd121f9e47d7f17c778b3e62a3722fad95d9
znuh <Zn000h@gmail.com>
2025-02-21 20:36:55 +0100

    use PB8 instead of PA13 for blinkenlight (PA13 is used by SWD after reset)

--- testing/testing.c
+++ testing/testing.c
@@ -2,12 +2,12 @@
 #include <libopencm3/stm32/gpio.h>
 
 int main(void) {
-	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_LOW, GPIO13);
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_LOW, GPIO8);
+	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
 	while(1) {
 		for(uint32_t v=100000;v;v--) {}
-		gpio_toggle(GPIOA, GPIO13);	
+		gpio_toggle(GPIOB, GPIO8);
 	}
 	return 0;
 }
