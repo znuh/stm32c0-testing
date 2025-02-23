5233001a91b71321af3f0b2848f917e0e9a27514
znuh <Zn000h@gmail.com>
2025-02-23 20:59:51 +0100

    fix stupid mistage (rcc_clock_setup w/ NULL ptr instead of actual clock struct /o\)

--- testing/testing.c
+++ testing/testing.c
@@ -1,5 +1,6 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 
 // for DFU bootrom reactivation
 #include <libopencm3/stm32/flash.h>
@@ -34,10 +35,38 @@ static void erase_page0(uint32_t safety_key) {
 	erase0_ram_func();
 }
 
+static void usart_init(void) {
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+	gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_HIGH, GPIO2);
+	gpio_set_af(GPIOA, GPIO_AF1, GPIO2);
+
+	rcc_periph_clock_enable(RCC_USART2);
+
+	usart_set_baudrate(USART2, 3000000);
+	usart_set_databits(USART2, 8);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_stopbits(USART2, USART_CR2_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+	usart_enable(USART2);
+}
+
+static void u2tx(const char *s) {
+	for(;*s;s++)
+		usart_send_blocking(USART2, *s);
+	usart_send_blocking(USART2, '\r');
+	usart_send_blocking(USART2, '\n');
+}
+
 int main(void) {
 	/* switch from 12MHz (div=4, default) to 48MHz (div=1) */
-	rcc_clock_setup(RCC_CLOCK_CONFIG_HSI_48MHZ);
+	rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_48MHZ]);
 
+	usart_init();
+	u2tx("HENLO!11\r\n");
+
+	/* blinkenlight and DFU button */
 	rcc_periph_clock_enable(RCC_GPIOB);
 	gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO9);
 	gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_LOW, GPIO8);
