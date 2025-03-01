06c12fb428762109efd394ac264ae9a650328ace
znuh <Zn000h@gmail.com>
2025-03-01 14:45:52 +0100

    bump libopencm3 version, remove page0 erase button (no longer needed)

--- testing/testing.c
+++ testing/testing.c
@@ -2,39 +2,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 
-// for DFU bootrom reactivation
-#include <libopencm3/stm32/flash.h>
-#include <libopencmsis/core_cm3.h>
-
-static void __attribute__( (section(".data#"), long_call, noinline) ) erase0_ram_func(void) {   /* extra # after section name mutes the asm warning m) */
-	uint32_t reg = FLASH_CR;
-	reg &= ~(FLASH_CR_PNB_MASK << FLASH_CR_PNB_SHIFT);
-	reg |= (0 & FLASH_CR_PNB_MASK)  << FLASH_CR_PNB_SHIFT;
-	reg |= FLASH_CR_PER;
-	reg |= FLASH_CR_STRT;
-
-	FLASH_CR = reg;
-
-	while(FLASH_SR & FLASH_SR_BSY) {} /* busywait */
-
-	FLASH_CR &= ~FLASH_CR_PER;
-
-	SCB_AIRCR = SCB_AIRCR_VECTKEY | SCB_AIRCR_SYSRESETREQ; /* trigger system reset via SCB */
-	while(1) {}
-}
-
-static void erase_page0(uint32_t safety_key) {
-	if(safety_key != 0xAA55) {
-		SCB_AIRCR = SCB_AIRCR_VECTKEY | SCB_AIRCR_SYSRESETREQ; /* trigger system reset via SCB */
-		while(1) {}
-		return;
-	}
-	cm_disable_interrupts();
-	flash_unlock();
-	flash_wait_for_last_operation();
-	erase0_ram_func();
-}
-
+/*
 static void usart_init(void) {
 	rcc_periph_clock_enable(RCC_GPIOA);
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
@@ -58,17 +26,18 @@ static void u2tx(const char *s) {
 	usart_send_blocking(USART2, '\r');
 	usart_send_blocking(USART2, '\n');
 }
+*/
 
 int main(void) {
 	/* switch from 12MHz (div=4, default) to 48MHz (div=1) */
 	rcc_clock_setup_in_hsi48_out_48mhz();
 
+/*
 	usart_init();
 	u2tx("HENLO!11\r\n");
-
-	/* blinkenlight and DFU button */
+*/
+	/* blinkenlight */
 	rcc_periph_clock_enable(RCC_GPIOB);
-	gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO9);
 	gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_LOW, GPIO8);
 	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
 
@@ -76,8 +45,6 @@ int main(void) {
 		for(uint32_t v=1000000;v;v--)
 			__asm__("nop");
 		gpio_toggle(GPIOB, GPIO8);
-		if(gpio_get(GPIOB, GPIO9))
-			erase_page0(0xAA55);
 	}
 	return 0;
 }
